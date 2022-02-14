0d31bd94de5ea2bdc0cc33211994b24182d0f9ed
rmagley <rachelmagley@gmail.com>
2022-02-14 14:57:17 -0500

    Add API call and return for My Site component

--- src/components/MySites.js
+++ src/components/MySites.js
@@ -1,15 +1,53 @@
 import React from 'react';
-import Create from './Create';
-import {Link} from 'react-router-dom'
+import { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import axios from 'axios';
 
 
 function MySites(props) {
-    return (
-			<div>
-				My Sites <br/>
-				<Link to='/createsite'>Create a new site!</Link>
-			</div>
-		);
-}
+	const [mySites, setMySites] = useState([]);
+	// const {id} = useParams();
 
+	useEffect(() => {
+		axios(`http://localhost:8000/api/reviews/`)
+			.then((res) => setMySites(res.data))
+			// .then (console.log(mySites))
+			.catch(console.error);
+	}, []);
+
+	if (!mySites.length) {
+		return <h1>Loading...</h1>;
+	}
+
+
+	return (
+		<div>
+			<div>
+				<h2>Visited</h2>
+				{mySites
+					.filter((visited) => visited.visited === true)
+					.map((visited) => (
+						<li key={visited.id}>{visited.site}</li>
+					))}
+			</div>
+			<div>
+				<h2>Favorites</h2>
+				{mySites
+					.filter((favorite) => favorite.favorite === true)
+					.map((favorite) => (
+						<li key={favorite.id}>{favorite.site}</li>
+					))}
+			</div>
+			<div>
+				<h2>Bucket List</h2>
+				{mySites
+					.filter((bucket_list) => bucket_list.bucket_list === true)
+					.map((bucket_list) => (
+						<li key={bucket_list.id}>{bucket_list.site}</li>
+					))}
+			</div>
+		</div>
+	);
+}
+	
 export default MySites;
